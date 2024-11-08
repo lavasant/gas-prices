659e72c9840f67d5c14a9bb02f45eaada10cf48b
James Pothen <69491494+jamespothen@users.noreply.github.com>
2024-11-08 16:58:44 -0500

    gets API call working

--- apicall.py
+++ apicall.py
@@ -2,9 +2,10 @@
 # https://www.eia.gov/opendata/browser/petroleum/pri/gnd?frequency=weekly&data=value;&sortColumn=period;&sortDirection=desc;
 # importing the requests library
 import requests
+import constants
 
 # api-endpoint
-URL = "https://api.eia.gov/v2/petroleum/pri/gnd/data/?frequency=weekly&data[0]=value&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000"
+URL = "https://api.eia.gov/v2/petroleum/pri/gnd/data/?frequency=weekly&data[0]=value&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000&api_key=" + constants.API_KEY
 
 # location given here
 #location = "delhi technological university"
