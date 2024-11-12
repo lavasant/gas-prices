e9c5a8e936ba102423828b80732822f3b63a67b2
James Pothen <69491494+jamespothen@users.noreply.github.com>
2024-11-12 15:40:06 -0500

    saves json response to csv file

--- apicall.py
+++ apicall.py
@@ -5,7 +5,6 @@ import requests
 import constants
 import json
 import csv
-csv_file_path = 'gas_prices.csv'
 
 # api-endpoint
 URL = "https://api.eia.gov/v2/petroleum/pri/gnd/data/?frequency=weekly&data[0]=value&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000&api_key=" + constants.API_KEY
@@ -19,11 +18,16 @@ response = r.json()
 API_Data = response["response"]["data"]
 keys = API_Data[0].keys()
 
-for price in API_Data:
-    print(price)
+# Specify the CSV file name
+filename = "gas_prices.csv"
 
-with open(csv_file_path, mode='w', newline='') as file:
-    # Create a csv.writer object
-    writer = csv.writer(file)
-    # Write data to the CSV file
-    writer.writerows(keys)
+# Writing to CSV file
+with open(filename, mode='w', newline='') as file:
+    # Create a DictWriter object
+    writer = csv.DictWriter(file, fieldnames=API_Data[0].keys())
+    # Write the header
+    writer.writeheader()
+    # Write the data rows
+    writer.writerows(API_Data)
+
+print(f"Data successfully written to {filename}")
