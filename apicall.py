094006a45d9b4b07b39d0bb0f49c4fb67c8ae455
James Pothen <69491494+jamespothen@users.noreply.github.com>
2024-11-12 15:20:35 -0500

    starts writing json to csv file

--- apicall.py
+++ apicall.py
@@ -4,6 +4,8 @@
 import requests
 import constants
 import json
+import csv
+csv_file_path = 'gas_prices.csv'
 
 # api-endpoint
 URL = "https://api.eia.gov/v2/petroleum/pri/gnd/data/?frequency=weekly&data[0]=value&sort[0][column]=period&sort[0][direction]=desc&offset=0&length=5000&api_key=" + constants.API_KEY
@@ -12,7 +14,16 @@ URL = "https://api.eia.gov/v2/petroleum/pri/gnd/data/?frequency=weekly&data[0]=v
 r = requests.get(url = URL)
 
 # extracting data in json format
-data = r.json()
+response = r.json()
 
-with open('data.json', 'w', encoding='utf-8') as f:
-    json.dump(data, f, ensure_ascii=False, indent=4)
+API_Data = response["response"]["data"]
+keys = API_Data[0].keys()
+
+for price in API_Data:
+    print(price)
+
+with open(csv_file_path, mode='w', newline='') as file:
+    # Create a csv.writer object
+    writer = csv.writer(file)
+    # Write data to the CSV file
+    writer.writerows(keys)
